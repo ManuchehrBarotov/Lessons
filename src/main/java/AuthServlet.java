928a79962dd02d54f22b42c0e30336eee34516cf
ManuchehrBarotov <manuchehrbarotov1998@gmail.com>
2022-03-04 10:58:53 +0300

    lessons

---
+++ src/main/java/AuthServlet.java
@@ -0,0 +1,37 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/authServlet")
+public class AuthServlet extends HttpServlet {
+
+       
+   
+    public AuthServlet() {
+    
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw =response.getWriter();
+	 if(request.isUserInRole("Person")) {
+		 pw.print("person in role");
+		 pw.print(request.getRemoteUser());
+		 pw.print(request.getUserPrincipal());
+	 }
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
